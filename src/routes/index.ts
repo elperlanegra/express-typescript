1f297facaed1e4b03934ddf4d20480d35c34aeda
Manuel Duarte <carlosd99m@gmail.com>
2022-03-19 17:51:12 -0600

    feat: Post and Comment Apis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import PingController from "../controllers/ping.controller";
 import UserRouter from "./user.router";
+import PostRouter from "./post.router";
+import CommentRouter from "./comment.router";
 
 const router = express.Router();
 
@@ -11,5 +13,7 @@ router.get("/ping", async (_req, res) => {
 });
 
 router.use("/users", UserRouter);
+router.use("/posts", PostRouter);
+router.use("/comments", CommentRouter);
 
 export default router;
