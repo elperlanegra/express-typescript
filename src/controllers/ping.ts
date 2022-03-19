1e6745fd6adfdb2a9e0dd4e53ad378ad9a298353
Manuel Duarte <carlosd99m@gmail.com>
2022-03-19 13:35:04 -0600

    feat: swagger integration

--- src/controllers/ping.ts
+++ src/controllers/ping.ts
@@ -1,8 +1,12 @@
+import { Get, Route } from 'tsoa';
+
 interface PingResponse {
     mesage: string;
 }
 
+@Route("ping")
 export default class PingController {
+    @Get("/")
     public async getMessage(): Promise<PingResponse> {
         return {
             mesage: 'pong',
